0feebf1b9a6c1baad15be2f16c93460f3101b3c2
Abhijeeth Padarthi <abhijeeth.padarthi@abhijeeth.padarthi-XXHWPG9WR9>
2024-09-04 14:38:42 -0700

    add issue labels to model

--- models/github/issue_concatenated.sql
+++ models/github/issue_concatenated.sql
@@ -2,9 +2,17 @@ WITH assignee_groups AS (
     SELECT 
         ia.issue_id,
         LISTAGG(u.login, ', ') WITHIN GROUP (ORDER BY u.login) AS assignees
-    FROM {{ source('github', 'issue_assignee') }} ia
-    LEFT JOIN {{ source('github', 'user') }} u ON ia.user_id = u.id
+    FROM {{ source('github','issue_assignee') }} ia
+    LEFT JOIN {{ source('github','user') }} u ON ia.user_id = u.id
     GROUP BY ia.issue_id
+),
+label_groups AS (
+    SELECT 
+        il.issue_id,
+        LISTAGG(l.name, ', ') WITHIN GROUP (ORDER BY l.name) AS labels
+    FROM {{ source('github','issue_label') }} il
+    LEFT JOIN {{ source('github','label') }} l ON il.label_id = l.id
+    GROUP BY il.issue_id
 ),
 issue_details AS (
     SELECT
@@ -16,21 +24,23 @@ issue_details AS (
         i.title,
         i.body, 
         u.login,
-        ag.assignees
-    FROM {{ source('github', 'issue') }} i
-    LEFT JOIN {{ source('github', 'user') }} u ON i.user_id = u.id
+        COALESCE(ag.assignees, 'No one assigned') as assignees,
+        COALESCE(lg.labels, 'None yet') as labels
+    FROM {{ source('github','issue') }} i
+    LEFT JOIN {{ source('github','user') }} u ON i.user_id = u.id
     LEFT JOIN assignee_groups ag ON i.id = ag.issue_id
-    
+    LEFT JOIN label_groups lg on i.id = lg.issue_id
     WHERE NOT i.pull_request 
 )
 SELECT
     id,
     CONCAT(
-        '# ISSUE : ',title,' (#',number,')\n\n',
-        'Opened by ',login,' on ',created_at,'\n',
-        'Assigned to : ',assignees,'\n',
-        'Last updated on ',updated_at,'\n',
-        'Issue is currently ',state,'\n\n',
-        '## Issue Body : \n',body
+        '# ', title, ' (#', number, ')\n\n',
+        'Opened by ', login, ' on ', created_at, '\n',
+        'Assigned to : ', assignees, '\n',
+        'Labels : ', labels, '\n',
+        'Last updated on ', updated_at, '\n',
+        'Issue is currently ', state, '\n\n',
+        '## DESCRIPTION : \n', body, '\n'
     ) AS issue_content
 FROM issue_details
