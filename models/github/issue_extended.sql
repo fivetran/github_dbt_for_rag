da21132bc81bdc83379b52854e3cb34a16ae29c6
Abhijeeth Padarthi <abhijeeth.padarthi@abhijeeth.padarthi-XXHWPG9WR9>
2024-09-04 13:57:28 -0700

    Update issue_extended.sql

--- models/github/issue_extended.sql
+++ models/github/issue_extended.sql
@@ -2,4 +2,4 @@ SELECT
     i.* ,
     ic.issue_content AS original_text
 FROM {{  source('github', 'issue') }} i 
-JOIN {{ ref('issue_concatenated')}} ic
+JOIN {{ ref('issue_concatenated') }} ic ON i.id = ic.id
